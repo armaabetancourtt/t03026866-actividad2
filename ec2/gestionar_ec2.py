4275c4caa68b07a46562883712d1f7b397a41949
Armando Betancourt <armabetancourt@gmail.com>
2026-04-06 01:28:23 +0000

    feat: listar instancias EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -7,18 +7,6 @@ def listar_instancias(ec2):
         for instancia in reserva['Instances']:
             print(f"ID: {instancia['InstanceId']} | Estado: {instancia['State']['Name']}")
 
-def iniciar_instancia(ec2, instance_id):
-    ec2.start_instances(InstanceIds=[instance_id])
-    print(f"Iniciando instancia {instance_id}")
-
-def detener_instancia(ec2, instance_id):
-    ec2.stop_instances(InstanceIds=[instance_id])
-    print(f"Deteniendo instancia {instance_id}")
-
-def terminar_instancia(ec2, instance_id):
-    ec2.terminate_instances(InstanceIds=[instance_id])
-    print(f"Terminando instancia {instance_id}")
-
 def main():
     if len(sys.argv) < 2:
         print("Uso: python gestionar_ec2.py <accion> [instance_id]")
@@ -27,28 +15,8 @@ def main():
     accion = sys.argv[1]
     ec2 = boto3.client('ec2')
 
-    try:
-        if accion == "listar":
-            listar_instancias(ec2)
-
-        elif accion in ["iniciar", "detener", "terminar"]:
-            if len(sys.argv) < 3:
-                print("Falta instance_id")
-                sys.exit(1)
-
-            instance_id = sys.argv[2]
-
-            if accion == "iniciar":
-                iniciar_instancia(ec2, instance_id)
-            elif accion == "detener":
-                detener_instancia(ec2, instance_id)
-            elif accion == "terminar":
-                terminar_instancia(ec2, instance_id)
-        else:
-            print("Acción no válida")
-
-    except Exception as e:
-        print(f"Error: {e}")
+    if accion == "listar":
+        listar_instancias(ec2)
 
 if __name__ == "__main__":
     main()
