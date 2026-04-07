0b21fedd8129c24bcf28b52f29d714fe0eb49c64
Armando Betancourt <armabetancourt@gmail.com>
2026-04-07 03:52:21 +0000

    feat: integración con EC2

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,10 @@ ACTION=$1
 INSTANCE_ID=$2
 DIRECTORY=$3
 BUCKET=$4
+
+if [ -z "$ACTION" ] || [ -z "$INSTANCE_ID" ]; then
+    echo "Uso: ./deploy.sh <accion> <instance_id>"
+    exit 1
+fi
+
+python3 ec2/gestionar_ec2.py $ACTION $INSTANCE_ID
